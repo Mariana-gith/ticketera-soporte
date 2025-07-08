12f38e23f4f04140620a4d2f89af0b3e8689be2b
Mariana <mariana.a.iamarino@gmail.com>
2025-07-08 12:32:46 -0300

    modified:   backend/package.json

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const tickets = require('./routes/tickets.js');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const tickets = require('./routes/tickets.js');
 const authRoutes = require('./routes/auth.js');
 const userRoutes = require('./routes/users.js');
 const inventarioRoutes = require('./routes/inventario');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
-// Conectar a la base de datos
-connectDB();
+// Configurar CORS para local y producción
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://ticketera-soporte-git-test-mariana-giths-projects.vercel.app']
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: allowedOrigins,
   credentials: true,
 }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// ✅ Rutas bien definidas
+// Rutas
 app.use('/api/tickets', tickets);
 app.use('/api', authRoutes);
-app.use('/api/users', userRoutes); // <- Esta era probablemente la ruta correcta
+app.use('/api/users', userRoutes);
 app.use('/api/inventario', inventarioRoutes);
+
+// Ruta de prueba
+app.get('/healthz', (req, res) => res.send('OK'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
