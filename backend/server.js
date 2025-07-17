433b4e78797b870847aec0cbb4efd08cc17d3e76
Mariana <mariana.a.iamarino@gmail.com>
2025-07-17 11:30:25 -0300

    modified:   backend/package.json

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,13 @@ app.get('/healthz', (req, res) => {
   res.send('OK');
 });
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist'))); // o ../client/build
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  });
+}
